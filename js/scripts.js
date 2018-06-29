b09d3ffaaf020557f5376329c8f68b895ca2ce17
Obure Joseph <oburejoseph@outlook.com>
2018-06-29 22:50:08 +0300

    add busines logic

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,26 @@ $(document).ready(function(){
     event.preventDefault();
   });
 });
+
+
+//Business Logic
+var countUp = function(countTo){
+  var result = [];
+  for(var i = 1; i <= countTo; i++){
+      var index = result.indexOf(i);
+      if(i % 15 === 0) {
+        result.splice(index, 0);
+        result.push('pingpong');
+      }else if(i % 5 === 0) {
+        result.splice(index, 0);
+        result.push('pong');
+      }else if(i % 3 === 0) {
+        result.splice(index, 0);
+        result.push('ping');
+      }else{
+        result.push(i);
+      }
+  }
+
+  return result;
+};
