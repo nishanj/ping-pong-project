4b2f8c7fec2a7099afdd6d49159731a8c1c424e1
Obure Joseph <oburejoseph@outlook.com>
2018-07-01 13:02:34 +0300

    changes in html/js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //Business Logic
-var countUp = function(countTo){
+var countUp = function(addTo){
   var result = [];
   // looping for
-  for(var i = 1; i <= countTo; i++){
+  for(var i = 1; i <= addTo; i++){
       var index = result.indexOf(i);
       // use of module
       if(i % 15 === 0) {
@@ -27,8 +27,8 @@ $(document).ready(function(){
   $("#userInput").submit(function(event){
     event.preventDefault();
     $("#output").empty();
-    var countTo = parseInt($("input#countTo").val());
-    var output = countUp(countTo);
+    var addTo= parseInt($("input#addTo").val());
+    var output = countUp(addTo);
 
     output.forEach(function(item){
       $("#output").append('<li>' + item + '</li>');
